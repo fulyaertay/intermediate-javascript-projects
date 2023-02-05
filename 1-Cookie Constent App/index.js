3d38ab6b6edc2e1b3324f365d00643ceac2e8aac
Fulya <fulya.ertay@gmail.com>
2023-02-06 01:14:54 +0300

    added pop-up close feature

--- 1-Cookie Constent App/index.js
+++ 1-Cookie Constent App/index.js
@@ -1,13 +1,18 @@
 /*
 Challenge:
-1. Take control of the 'modal' div with JavaScript.
-2. Swap out our console.log for a line of code
-   which will change the CSS 'display' property
-   of our modal to 'inline'.
+1. Take control of the close button.
+2. Use an event listener to set the display 
+   property of the modal to 'none' when the
+   close button is clicked.
 */
 
 const modal = document.getElementById('modal')
-
+const modalCloseBtn = document.getElementById('modal-close-btn')
+ 
 setTimeout(function(){
     modal.style.display = 'inline'
 }, 1500)
+
+modalCloseBtn.addEventListener('click', function(){
+    modal.style.display = 'none'
+})
