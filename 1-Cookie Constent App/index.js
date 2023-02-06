b6f3fe8c34a86ddd6d44a2a5d998b5001f56d4a9
Fulya <fulya.ertay@gmail.com>
2023-02-06 14:24:06 +0300

    added form listener

--- 1-Cookie Constent App/index.js
+++ 1-Cookie Constent App/index.js
@@ -1,18 +1,28 @@
-/*
-Challenge:
-1. Take control of the close button.
-2. Use an event listener to set the display 
-   property of the modal to 'none' when the
-   close button is clicked.
-*/
-
 const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
- 
+const consentForm = document.getElementById('consent-form')
+
 setTimeout(function(){
-    modal.style.display = 'block';
+    modal.style.display = 'inline'
 }, 1500)
 
 modalCloseBtn.addEventListener('click', function(){
-    modal.style.display = 'none';
+    modal.style.display = 'none'
 })
+
+
+consentForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    console.log('form submitted')
+})
+
+/*
+Challenge:
+1. Take control of the form element. 
+2. Add an eventListener to the form to listen for a
+   "submit" event.
+3. When a user clicks "accept", prevent the default
+   behaviour that triggers the refresh.
+4. Log out "form submitted".
+*/  
+
