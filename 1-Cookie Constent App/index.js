f38187b51b77366defedc30e55c07ff9700118ca
Fulya <fulya.ertay@gmail.com>
2023-02-06 12:56:04 +0300

    edited js file

--- 1-Cookie Constent App/index.js
+++ 1-Cookie Constent App/index.js
@@ -1,10 +1,4 @@
-/*
-Challenge:
-1. Take control of the close button.
-2. Use an event listener to set the display 
-   property of the modal to 'none' when the
-   close button is clicked.
-*/
+
 
 const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
