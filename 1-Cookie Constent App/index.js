e907cce8b61426062b1f83fd3b677719ffbc6e24
Fulya <fulya.ertay@gmail.com>
2023-02-06 13:54:59 +0300

    added form on pop-up

--- 1-Cookie Constent App/index.js
+++ 1-Cookie Constent App/index.js
@@ -1,56 +1,18 @@
-<html>
+/*
+Challenge:
+1. Take control of the close button.
+2. Use an event listener to set the display 
+   property of the modal to 'none' when the
+   close button is clicked.
+*/
 
-	<head>
-		<link rel="stylesheet" href="index.css">
-		<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
-	</head>
+const modal = document.getElementById('modal')
+const modalCloseBtn = document.getElementById('modal-close-btn')
+ 
+setTimeout(function(){
+    modal.style.display = 'block';
+}, 1500)
 
-	<body>
-		<main>
-			<section class="section-container">
-				<h1>Learning to code? This is the most important thing you will ever read!</h1>
-
-				<ul>
-					<li class="li-green">💰 Land a job with a 6 figure salary</li>
-					<li class="li-yellow">💻 Master JavaScript in 3 hours</li>
-					<li class="li-blue">⚛️ Learn React in 10 minutes</li>
-					<li class="li-pink">☕ Be a NodeJS Dev by lunchtime</li>
-				</ul>
-
-				<h3>My subliminal learning technique worked for Mark Zuckerberg and Albert Einstein, and it can work for you!</h3>
-				<img src="images/bugatti.jpg">
-				<p>Want a great job in tech? Want a six-figure salary? Then you have come to the right place. I am going to tell you all you need to know to get all of that and much more, without you lifting a finger. You can learn passively. Just sit back, close your eyes, and let the sound of my voice make the code sink deep into your brain. </p>
-			</section>
-		</main>
-		<div class="modal" id="modal">
-			<div class="close-modal-btn-container">
-				<button class="modal-close-btn" id="modal-close-btn">X</button>
-			</div>
-			<div class="modal-inner" id="modal-inner">
-				<h2>We ❤️ Your Data!</h2>
-				<p class="modal-text" id="modal-text">We have a strict policy on tracking and spamming: we'll definitely track you, and we'll definitely spam you. To use this site, enter your name and email address and accept our ridiculous terms and conditions.</p>	
-				
-<!-- 
-Challenge: 
-1. Create a form using form tags.
-2. Add two inputs: one for name and one 
-   for email address.
-3. Give each input "type", "name" and 
-   "placeholder" properties. You can see the
-   placeholder text we need in the slide. 
--->	
-
-			<form>
-				<input type="text" name="fullName" placeholder="Enter your full name">
-				<input type="email" name="email" placeholder="Enter your email">
-			</form>
-				
-				
-				
-							
-			</div>
-		</div>
-		<script src="index.js"></script>
-	</body>
-
-</html>
+modalCloseBtn.addEventListener('click', function(){
+    modal.style.display = 'none';
+})
