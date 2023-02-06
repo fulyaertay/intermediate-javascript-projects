8b2c9643444e23a7e5e861954f780a1f49e2f1c5
Fulya <fulya.ertay@gmail.com>
2023-02-06 14:41:05 +0300

    added images

--- 1-Cookie Constent App/index.js
+++ 1-Cookie Constent App/index.js
@@ -1,6 +1,7 @@
 const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
 const consentForm = document.getElementById('consent-form')
+const modalText = document.getElementById('modal-text')
 
 setTimeout(function(){
     modal.style.display = 'inline'
@@ -10,19 +11,29 @@ modalCloseBtn.addEventListener('click', function(){
     modal.style.display = 'none'
 })
 
-
 consentForm.addEventListener('submit', function(e){
     e.preventDefault()
-    console.log('form submitted')
-})
-
+    modalText.innerHTML = `
+    <div class="modal-inner-loading">
+        <img src="images/loading.svg" class="loading">
+        <p id="uploadText">
+            Uploading your data to the dark web...
+        </p>
+    </div>`
 /*
 Challenge:
-1. Take control of the form element. 
-2. Add an eventListener to the form to listen for a
-   "submit" event.
-3. When a user clicks "accept", prevent the default
-   behaviour that triggers the refresh.
-4. Log out "form submitted".
-*/  
+1. Take control of the "modal-text" element. 
+2. Make it so that when a user clicks on 
+   the accept button, the HTML string below
+   is inserted into the modal-text div.
+
+<div class="modal-inner-loading">
+    <img src="images/loading.svg" class="loading">
+    <p id="uploadText">
+        Uploading your data to the dark web...
+    </p>
+</div>
+*/ 
+})
+ 
 
