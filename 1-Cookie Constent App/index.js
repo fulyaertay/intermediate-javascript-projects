f24c20950db161c5b30f66f5d19dbd03cc706425
Fulya <fulya.ertay@gmail.com>
2023-02-06 20:01:24 +0300

    added disabled icon on pop-up

--- 1-Cookie Constent App/index.js
+++ 1-Cookie Constent App/index.js
@@ -16,17 +16,6 @@ consentForm.addEventListener('submit', function(e){
     
     const consentFormData = new FormData(consentForm)
     const fullName = consentFormData.get('fullName')
-
-/*   
-Challenge: 
-1. Create a const to store the user's name and
-   use a FormData method to extract the 
-   submitted name from the FormData object.
-2. Insert the user's name into the HTML string
-   that contains the final message we show our
-   users.
-*/ 
-    
     
     modalText.innerHTML = `
     <div class="modal-inner-loading">
@@ -39,7 +28,13 @@ Challenge:
         Making the sale...`
     }, 1500)
     
-    
+/*   
+Challenge: 
+1. Make the button that closes the modal disabled.
+2. Make that button become usable when the final 
+   modal message has been displayed to the user.
+*/ 
+
     setTimeout(function(){
         document.getElementById('modal-inner').innerHTML = `
         <h2>Thanks <span class="modal-display-name">${fullName}</span>, you sucker! </h2>
@@ -48,6 +43,7 @@ Challenge:
             <img src="images/pirate.gif">
         </div>
     `
+    modalCloseBtn.disabled = false
     }, 3000)
   
 }) 
