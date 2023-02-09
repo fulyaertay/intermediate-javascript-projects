c1db5afb9c44fffeed2c34f19bc0a3928793edcd
Fulya <fulya.ertay@gmail.com>
2023-02-09 22:57:01 +0300

    get the id of clicked radio button

--- 2-meme-picker-app/index.js
+++ 2-meme-picker-app/index.js
@@ -1,22 +1,27 @@
-import { catsData } from './data.js'
+import { catsData } from '/data.js'
 
 const emotionRadios = document.getElementById('emotion-radios')
 
-function getEmotionsArray(cats) {
-    const emotionsArray = []
+/*
+Challenge:
+1. Add an eventListener to emotionRadios that will listen 
+   out for any *change* in our radio buttons. When it detects
+   a change, it should log out the id of the element that 
+   was selected.
+⚠️️ ️T️h️is won't work if the eventListener is listening out for a 
+   'click'. Google what event to listen for - I've already 
+   given you a clue!
+*/
 
-    for (let cat of cats) {
-        for (let emotion of cat.emotionTags) {
-            /*
-            Challenge:
-            1. Refactor this nested for of so that an 
-               emotion is only pushed to emotionsArray
-               if it is not already in emotionsArray.
-               Extra kudos if you use the "logical not"
-               operator - feel free to google it!
-            */
+emotionRadios.addEventListener('change', function(e){
+    console.log(e.target.id)
+})
 
-            if (!emotionsArray.includes(emotion)) {
+function getEmotionsArray(cats){
+    const emotionsArray = []    
+    for (let cat of cats){
+        for (let emotion of cat.emotionTags){
+            if (!emotionsArray.includes(emotion)){
                 emotionsArray.push(emotion)
             }
         }
@@ -25,11 +30,11 @@ function getEmotionsArray(cats) {
 }
 
 
-function renderEmotionsRadios(cats) {
-
+function renderEmotionsRadios(cats){
+        
     let radioItems = ``
     const emotions = getEmotionsArray(cats)
-    for (let emotion of emotions) {
+    for (let emotion of emotions){
         radioItems += `
         <div class="radio">
             <label for="${emotion}">${emotion}</label>
