e093614435dd3e37d9eaa5403a6cd7e2842ab15c
Fulya <fulya.ertay@gmail.com>
2023-02-10 00:31:59 +0300

    added style on radio buttons

--- 2-meme-picker-app/index.js
+++ 2-meme-picker-app/index.js
@@ -2,20 +2,18 @@ import { catsData } from './data.js'
 
 const emotionRadios = document.getElementById('emotion-radios')
 
+emotionRadios.addEventListener('change', highlightCheckedOption)
+
+function highlightCheckedOption(e){
+    document.getElementById(e.target.id).parentElement.classList.add('highlight')
 /*
 Challenge:
-1. Add an eventListener to emotionRadios that will listen 
-   out for any *change* in our radio buttons. When it detects
-   a change, it should log out the id of the element that 
-   was selected.
-⚠️️ ️T️h️is won't work if the eventListener is listening out for a 
-   'click'. Google what event to listen for - I've already 
-   given you a clue!
+1. Change the code in line 8 so we add the 
+   'highlight' class to the parent of the 
+   selected input radio.
 */
-
-emotionRadios.addEventListener('change', function(e){
-    console.log(e.target.id)
-})
+}
+ 
 
 function getEmotionsArray(cats){
     const emotionsArray = []    
