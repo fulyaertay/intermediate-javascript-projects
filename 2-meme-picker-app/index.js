09d8ef96cd0d7193be4374dc0ae880f04e3f9917
Fulya <fulya.ertay@gmail.com>
2023-02-09 22:00:11 +0300

    rendered radio inputs

--- 2-meme-picker-app/index.js
+++ 2-meme-picker-app/index.js
@@ -13,10 +13,36 @@ function getEmotionsArray(cats){
 }
 
 function renderEmotionsRadios(cats){
+    
     let radioItems = ``
     const emotions = getEmotionsArray(cats)
     for (let emotion of emotions){
-        radioItems += `<p>${emotion}</p>`
+/*
+Challenge:
+1. Swap out `<p>${emotion}</p>` for HTML
+   that will render a radio input for each
+   emotion. Remember to use "type", "id", 
+   "value", and "name" properties on each radio.
+   ("id" and "value" can both be set to the
+   "emotion").
+2. Remember to give each radio a label.
+   (What property does a label need?)
+3. Enclose each individual radio input in this div:
+   <div class="radio">
+      **RADIO HERE**
+   </div>
+*/ 
+        radioItems += `
+        <div class="radio">
+            <label for="${emotion}">${emotion}</label>
+            <input
+                type="radio"
+                id="${emotion}"
+                value="${emotion}"
+                name="emotions"
+                >
+        </div>
+        ` 
     }
     emotionRadios.innerHTML = radioItems
 }
