d639080f01dade1236c2cf83aa3125e04b10f32b
Fulya <fulya.ertay@gmail.com>
2023-02-10 16:42:20 +0300

    connected image btn

--- 2-meme-picker-app/index.js
+++ 2-meme-picker-app/index.js
@@ -1,24 +1,34 @@
 import { catsData } from './data.js'
 
 const emotionRadios = document.getElementById('emotion-radios')
+const getImageBtn = document.getElementById('get-image-btn')
+
+/*
+Challenge:
+1. Set up an eventlistener which calls a new
+   function called "getMatchingCatsArray" when
+   the "Get Image" button is clicked.
+2. getMatchingCatsArray should save the value
+   of the checked radio input to a const and 
+   log out that const.
+*/
 
 emotionRadios.addEventListener('change', highlightCheckedOption)
 
+getImageBtn.addEventListener('click', getMatchingCatsArray)
+
 function highlightCheckedOption(e){
-/*
-Challenge:
-1. Create an array of all items that have 
-   the "radio" class.
-2. Iterate over the array and remove the 
-   "highlight" class from each one.
-*/ 
     const radios = document.getElementsByClassName('radio')
-    
     for (let radio of radios){
         radio.classList.remove('highlight')
     }
-
+    // remove all instances of the highlight class
     document.getElementById(e.target.id).parentElement.classList.add('highlight')
+}
+
+function getMatchingCatsArray(){
+    const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
+    console.log(selectedEmotion)
 }
 
 function getEmotionsArray(cats){
