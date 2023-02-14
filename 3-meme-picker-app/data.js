b3c95b9f4ed5fde6a47278022643de96f20d836e
Fulya <fulya.ertay@gmail.com>
2023-02-14 16:23:02 +0300

    refactor data.js

--- 3-meme-picker-app/data.js
+++ 3-meme-picker-app/data.js
@@ -1,10 +1,5 @@
 export const catsData = [
-    {
-        emotionTags: ["moody"],
-        isGif: false,
-        image: "angry.jpeg",
-        alt: "A cat looking moody",
-    },
+   
     {
         emotionTags: ["moody", "insomniac"],
         isGif: false,
