669bba8249c4962e7a1e395ed7889318bdd3d6fd
Fulya <fulya.ertay@gmail.com>
2023-02-14 23:51:31 +0300

    added tweet boilerplate

--- 4-twitter-clone/index.js
+++ 4-twitter-clone/index.js
@@ -4,10 +4,73 @@ const tweetBtn = document.getElementById('tweet-btn')
 
 tweetBtn.addEventListener('click', function(){
     console.log(tweetInput.value)
-/*
-Challenge
-1. Have the eventListener's function
-   log out the contents of the textarea
-   when the Tweet button is clicked.
-*/
 })
+
+function getFeedHtml(){
+    
+    let feedHtml = ``
+    
+    for (let tweet of tweetsData){
+        feedHtml += `
+<div class="tweet">
+    <div class="tweet-inner">
+        <img src="${tweet.profilePic}" class="profile-pic">
+        <div>
+            <p class="handle">${tweet.handle}</p>
+            <p class="tweet-text">${tweet.tweetText}</p>
+            <div class="tweet-details">
+                <span class="tweet-detail">
+                    ${tweet.replies.length}
+                </span>
+                <span class="tweet-detail">
+                    ${tweet.likes}
+                </span>
+                <span class="tweet-detail">
+                    ${tweet.retweets}
+                </span>
+            </div>   
+        </div>            
+    </div>
+</div>`
+    }
+return feedHtml
+/*
+Challenge:
+1. Use a "for of" to iterate over the data and 
+   create HTML string for each tweet using the 
+   boilerplate below. Replace UPPERCASE text
+   with data from the tweets. 
+2. Store this HTML in a let called "feedHtml".
+3. Log out feedHtml.
+4. Call getFeedHtml to check it's working.
+*/  
+}
+
+getFeedHtml()
+
+
+
+/*
+<div class="tweet">
+    <div class="tweet-inner">
+        <img src="URL OF PROFILE PIC" class="profile-pic">
+        <div>
+            <p class="handle">TWEET HANDLE</p>
+            <p class="tweet-text">TWEET TEXT</p>
+            <div class="tweet-details">
+                <span class="tweet-detail">
+                    NUMBER OF REPLIES
+                </span>
+                <span class="tweet-detail">
+                    NUMBER OF LIKES
+                </span>
+                <span class="tweet-detail">
+                    NUMBER OF RETWEETS
+                </span>
+            </div>   
+        </div>            
+    </div>
+</div>
+
+*/
+
