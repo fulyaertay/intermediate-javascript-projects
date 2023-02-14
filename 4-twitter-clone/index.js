1a9dedfc9f26d2d2970c17a52e550316df83c6bc
Fulya <fulya.ertay@gmail.com>
2023-02-14 23:39:48 +0300

    added tweet input area

--- 4-twitter-clone/index.js
+++ 4-twitter-clone/index.js
@@ -1,1 +1,13 @@
-import { tweetsData } from "./data";
+import { tweetsData } from './data.js'
+const tweetInput = document.getElementById('tweet-input')
+const tweetBtn = document.getElementById('tweet-btn')
+
+tweetBtn.addEventListener('click', function(){
+    console.log(tweetInput.value)
+/*
+Challenge
+1. Have the eventListener's function
+   log out the contents of the textarea
+   when the Tweet button is clicked.
+*/
+})
