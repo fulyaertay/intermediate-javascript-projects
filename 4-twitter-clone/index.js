0e01f89243690ce3f60eee5671b1241715357923
Fulya <fulya.ertay@gmail.com>
2023-02-15 18:24:26 +0300

    added replies

--- 4-twitter-clone/index.js
+++ 4-twitter-clone/index.js
@@ -62,14 +62,41 @@ function getFeedHtml(){
             retweetIconClass = 'retweeted'
         }
         
+        let repliesHtml = ''
+        
+        if(tweet.replies.length > 0){
+            tweet.replies.forEach(function(reply){
+                repliesHtml+=`
+<div class="tweet-reply">
+    <div class="tweet-inner">
+        <img src="${reply.profilePic}" class="profile-pic">
+            <div>
+                <p class="handle">${reply.handle}</p>
+                <p class="tweet-text">${reply.tweetText}</p>
+            </div>
+        </div>
+</div>
+`
+            })
 /*
 Challenge:
-1. Use an if statement to set the value of 
-   'retweetIconClass' to the string 
-   'retweeted' if the tweet has been retweeted. 
-2. In the retweet icon tag, add 'retweetIconClass' 
-   to the list of classes.
+1. If a tweet has replies, iterate through the replies
+   and wrap each one in the HTML template provided below. 
+   Make sure to replace words in UPPERCASE with data from 
+   the tweet. On each iteration, add this HTML to repliesHtml.
+   
+<div class="tweet-reply">
+    <div class="tweet-inner">
+        <img src="PROFILE PIC" class="profile-pic">
+            <div>
+                <p class="handle">HANDLE</p>
+                <p class="tweet-text">TWEET TEXT</p>
+            </div>
+        </div>
+</div>
 */
+        }
+        
           
         feedHtml += `
 <div class="tweet">
@@ -100,8 +127,18 @@ Challenge:
             </div>   
         </div>            
     </div>
+    <div class="hidden" id="replies-${tweet.uuid}">
+        ${repliesHtml}
+    </div>   
 </div>
 `
+/*
+Challenge:
+2. Place repliesHtml in its parent div remembering 
+   to update that divs id.
+*/
+
+
    })
    return feedHtml 
 }
