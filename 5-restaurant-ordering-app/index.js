96526d2f9281a0b6c0a5032c360d9aa3c8a07929
Fulya <fulya.ertay@gmail.com>
2023-02-19 01:31:06 +0300

    added remove button on orders

--- 5-restaurant-ordering-app/index.js
+++ 5-restaurant-ordering-app/index.js
@@ -2,17 +2,10 @@ import { menuArray } from "./data.js"
 const orderDetails=document.querySelector(".order-details")
 const listMenu=document.querySelector("#list-orders")
 
-document.addEventListener("click",function(e){
-    if(e.target.dataset.add){
-        handleOrderDetails(e.target.dataset.add)
 
-    }
-})
 
 function  handleOrderDetails(menuId){
-   const menuObject= menuArray.filter(function(menu){
-        return menu.id == menuId
-    })[0]
+  
     orderDetails.classList.toggle("hidden")
 
  
@@ -22,6 +15,7 @@ function  handleOrderDetails(menuId){
 
 function renderMenu(){
     let innerHtml=""
+    let orderDetail=""
     menuArray.forEach(function(menu){
         innerHtml=`
         <div class="menu">
@@ -40,9 +34,41 @@ function renderMenu(){
       </div>`
       listMenu.innerHTML+=innerHtml;
 
+   
 
 
     })
+
+    document.addEventListener("click",function(e){
+        if(e.target.dataset.add){
+           let orderId=e.target.dataset.add;
+           const menuObject= menuArray.filter(function(menu){
+            return menu.id == orderId
+        })[0]
+
+        listMenu.innerHTML+=`
+        <h4>Your Order</h4>
+        <div class="orders">
+          <div>${menuObject.name} <span class="remove">remove</span></div>
+          <div class="order-price">${menuObject.price}$</div>
+        </div>
+        <hr />
+        <div class="orders">
+          <div>Total Price</div>
+          <div class="order-price">${menuObject.price}$</div>
+        </div>
+        <div class="orders">
+          <button class="completeBtn">Complete Order</button>
+        </div>`
+
+        
+
+    
+        }
+    })
+
+
+    
    
   
 }
