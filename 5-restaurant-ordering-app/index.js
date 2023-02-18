7eb371c05231b3b10463d85519d513a5e5a578f2
Fulya <fulya.ertay@gmail.com>
2023-02-18 22:21:40 +0300

    rendered menus to html

--- 5-restaurant-ordering-app/index.js
+++ 5-restaurant-ordering-app/index.js
@@ -1,1 +1,28 @@
 import { menuArray } from "./data.js"
+
+const listMenu=document.querySelector("#list-orders")
+
+function renderMenu(){
+    let innerHtml=""
+    menuArray.forEach(function(menu){
+        innerHtml=`<div class="menuItems">
+        <div class="menu">
+          <div class="logo">${menu.emoji}</div>
+          <div class="order-content">
+            <div class="food-name">${menu.name}</div>
+    
+            <div class="ingredients">${menu.ingredients}</div>
+            <div class="price">${menu.price}$</div>
+          </div>
+          <div class="add-cart">
+            <span><i class="fa fa-plus-circle" data-add-chart=""></i></span>
+          </div>
+        </div>
+      </div>`
+      listMenu.innerHTML+=innerHtml;
+
+    })
+   
+  
+}
+renderMenu()
