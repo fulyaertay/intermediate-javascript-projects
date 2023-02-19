ef9bc7362d13b8d3a4abaf564b943df224cf9b94
Fulya <fulya.ertay@gmail.com>
2023-02-19 18:39:57 +0300

    added form after clicked pay button

--- 5-restaurant-ordering-app/index.js
+++ 5-restaurant-ordering-app/index.js
@@ -1,77 +1,93 @@
-import { menuArray } from "./data.js"
-const orderDetails=document.querySelector(".order-details")
-const listMenu=document.querySelector("#list-orders")
+import { menuArray } from './data.js'
 
-document.addEventListener("click",function(e){
+const orderBtn = document.getElementById('order-btn')
+const yourOrderTitle = document.getElementById('your-order-title')
+const payBtn = document.getElementById('pay-btn')
+
+let sumOrderPrice = 0
+
+document.addEventListener('click', function(e){
     if(e.target.dataset.add){
-
-    renderOrders(e.target.dataset.add)
-    orderDetails.classList.remove("hidden")
-
+        handleAddClick(e.target.dataset.add)
     }
 })
 
+orderBtn.addEventListener('click', function(){
+    document.getElementById('card-deatils').style.display = 'block'
+})
 
+payBtn.addEventListener('click', function(e){
+    e.preventDefault()
+    document.getElementById('card-deatils').style.display = 'none'
+    document.getElementById('your-order').style.display = 'none'
+    document.getElementById('thank-you').style.display = 'block'
+})
 
-function renderOrders(orders){
-
-    const menuObject= menuArray.filter(function(menu){
-     return menu.id == orders
- })[0]
-
-
-    orderDetails.innerHTML+=`
-    <h4>Your Order</h4>
-    <div class="orders">
-      <div>${menuObject.name} <span class="remove">remove</span></div>
-      <div class="order-price">${menuObject.price}$</div>
-    </div>
-    <hr />
-    <div class="orders">
-      <div>Total Price</div>
-      <div class="order-price">${menuObject.price}$</div>
-    </div>
-    <div class="orders">
-      <button class="completeBtn">Complete Order</button>
-    </div>`
-
-
-   
-
-}
-
-
-function renderMenu(){
-    let innerHtml=""
-    let orderDetail=[]
-    menuArray.forEach(function(menu){
-        innerHtml=`
-        <div class="menu">
-          <div class="logo">${menu.emoji}</div>
-          <div class="order-content">
-            <div class="food-name">${menu.name}</div>
-            <div class="ingredients">${menu.ingredients}</div>
-            <div class="price">${menu.price}$</div>
-          </div>
-          <div class="add-cart">
-            <span><i class="fa fa-plus-circle" data-add=${menu.id}></i></span>
-          </div>
-
-        
-       
-      </div>`
-      listMenu.innerHTML+=innerHtml;
-
-   
-
-
-    })
-
-
-
-
+function handleAddClick(addId){
+    let orderHtml = ''
     
-   
-  
+    const obj = menuArray.filter(function(add, tt){
+        return add.id == addId
+    })[0]
+    let total = ''
+    orderHtml += `
+        <div class="order-section-price">
+            <div class="order-name"> ${obj.name} <span class="remove-order">remove</span></div>
+            <div class="order-price">$${obj.price} </div>
+        </div>
+        
+    `
+    sumOrderPrice += obj.price
+    
+    if(sumOrderPrice > 0) {
+        orderBtn.style.display = 'block'
+        yourOrderTitle.style.display = 'block'
+    }
+    
+    total = `
+         <div class="total-div">
+            <div class="order-name total">Total price:</div>
+            <div class="order-price">$${sumOrderPrice} </div>
+        </div
+        
+        `
+    
+    // console.log(sumOrderPrice)
+        document.getElementById('single-order').innerHTML += orderHtml
+        document.getElementById('total').innerHTML = total
 }
-renderMenu()
+
+function getFeed(){
+    let feedHtml = ''
+    
+    menuArray.forEach(function(feed){
+       
+        feedHtml +=`
+            <div class="singel-menu">
+                <div class="menu-content">
+                    <div class="emoji">${feed.emoji}</div>
+                    <div>
+                        <p class="name">${feed.name}</p>
+                        <p class="ingredients">${feed.ingredients}</p>
+                        <p class="price">$${feed.price}</p>
+                    </div>
+                </div>
+                <img src="./images/add-btn.png" alt="add btn" class="add-btn" data-add=" ${feed.id}"/>
+            </div>
+        `
+    })
+    return feedHtml
+}
+
+function getTotal(){
+    let price = 0
+    
+    
+}
+
+function render(){
+    document.getElementById('menu-content').innerHTML = getFeed()
+    
+}
+
+render()
